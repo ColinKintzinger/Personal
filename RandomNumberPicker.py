8b5b10bd00814dea93d4c339086c063f89351238
ColinKintzinger <157931650+ColinKintzinger@users.noreply.github.com>
2024-05-15 14:37:39 -0500

    made a random number guesser game

---
+++ RandomNumberPicker.py
@@ -0,0 +1,17 @@
+import random
+class MyClass:
+    isCorrect=False
+    a=int(input("Enter the lower limit: "))
+    b=int(input("Enter the upper limit: "))
+    c=random.randint(a,b)
+    print(c)
+    while isCorrect!=True:
+        d=int(input("Enter a number to guess: "))
+        if d==c:
+            print("You have guessed the correct number")
+            isCorrect=True
+        elif d>c:
+            print("The number you have guessed is greater than the correct number")
+        else:
+            print("The number you have guessed is smaller than the correct number")
+        
