65ae114d0df4208f407c5dbb579d0476cb424168
ColinKintzinger <157931650+ColinKintzinger@users.noreply.github.com>
2024-05-15 14:39:50 -0500

    added comments

--- RandomNumberPicker.py
+++ RandomNumberPicker.py
@@ -1,17 +1,22 @@
 import random
+
 class MyClass:
-    isCorrect=False
-    a=int(input("Enter the lower limit: "))
-    b=int(input("Enter the upper limit: "))
-    c=random.randint(a,b)
+    """
+    A class that allows the user to guess a random number within a given range.
+    """
+
+    isCorrect = False
+    a = int(input("Enter the lower limit: "))
+    b = int(input("Enter the upper limit: "))
+    c = random.randint(a, b)
     print(c)
-    while isCorrect!=True:
-        d=int(input("Enter a number to guess: "))
-        if d==c:
+
+    while isCorrect != True:
+        d = int(input("Enter a number to guess: "))
+        if d == c:
             print("You have guessed the correct number")
-            isCorrect=True
-        elif d>c:
+            isCorrect = True
+        elif d > c:
             print("The number you have guessed is greater than the correct number")
         else:
             print("The number you have guessed is smaller than the correct number")
-        
