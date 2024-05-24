0227c2c7369d50f0155dec673f9107d6e9951fe0
ColinKintzinger <157931650+ColinKintzinger@users.noreply.github.com>
2024-05-24 11:50:35 -0500

    got some code down

--- LeetCode/easy/SpecialArrays.py
+++ LeetCode/easy/SpecialArrays.py
@@ -1,2 +1,11 @@
+from typing import List
 class Solution:
-    
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        if len(nums) < 1:
+            return True
+        for i in range(len(nums)-1): 
+            if nums[i] % 2 == 0 and nums[i+1] % 2 ==1 or nums[i] % 2 == 1 and nums[i+1] % 2 == 0:
+                return True
+        return False
+nums= List[2,1,4]
+ret = Solution().isArraySpecial(nums)
