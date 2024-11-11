4d69820db858e0ba040341db6df579682a567b71
ColinKintzinger <157931650+ColinKintzinger@users.noreply.github.com>
2024-11-11 08:59:53 -0600

    redid problem 2

--- Project_Euler/problem2.py
+++ Project_Euler/problem2.py
@@ -1,13 +1,13 @@
-curr=2
-prev=1
-sum=0 
-tmp=0
-def fib(prev, curr)->int:
-    return prev+curr
-while curr<=4000000:
+curr=1
+prev=0
+sum=0
+count = 0
+while count<10:
     if curr%2==0:
         sum+=curr
-    prev=curr
-    curr=fib(curr, prev)
+        count+=1
+    temp=curr
+    curr+=prev
+    prev=temp
 print(sum)
 
