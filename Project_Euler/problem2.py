e939ed184f0e59560e0b6f8370fff1b9c0d99eaa
ColinKintzinger <157931650+ColinKintzinger@users.noreply.github.com>
2024-11-11 09:04:58 -0600

    finished problem 2

--- Project_Euler/problem2.py
+++ Project_Euler/problem2.py
@@ -2,12 +2,17 @@ curr=1
 prev=0
 sum=0
 count = 0
-while count<10:
+while sum<4000000:
     if curr%2==0:
         sum+=curr
         count+=1
-    temp=curr
-    curr+=prev
-    prev=temp
+        temp=curr
+        curr+=prev
+        prev=temp
+    else:
+        count+=1
+        temp=curr
+        curr+=prev
+        prev=temp
 print(sum)
 
