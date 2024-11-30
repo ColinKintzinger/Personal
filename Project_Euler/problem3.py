7f9e2c0ca6f967306dd15378702b038669f365aa
ColinKintzinger <157931650+ColinKintzinger@users.noreply.github.com>
2024-11-29 19:08:49 -0600

    solved another project euler problem
    along with starting the file for the next problem.

---
+++ Project_Euler/problem3.py
@@ -0,0 +1,10 @@
+def prime_number():
+    n = 600851475143
+    i = 2
+    while i * i < n:
+        while n % i == 0:
+            n = n / i
+        i = i + 1
+    return n
+
+print(prime_number())
